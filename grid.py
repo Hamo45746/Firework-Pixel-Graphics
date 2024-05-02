bc0ffaa57fdfb36ffa5a818c98414b87911af958
Hamo45746 <hamo45746@outlook.com>
2024-05-02 17:42:04 +1000

    Split into more files for new firework sim classes.

--- grid.py
+++ grid.py
@@ -2,33 +2,16 @@ class Grid:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.grid = [0] * (width * height)
+        self.cells = [[0 for _ in range(height)] for _ in range(width)]
+
+    def set_cell(self, x, y, value):
+        if 0 <= x < self.width and 0 <= y < self.height:
+            self.cells[x][y] = value
+
+    def get_cell(self, x, y):
+        if 0 <= x < self.width and 0 <= y < self.height:
+            return self.cells[x][y]
+        return None
 
     def clear(self):
-        self.grid = [0] * (self.width * self.height)
-
-    def set(self, x, y, color):
-        if 0 <= x < self.width and 0 <= y < self.height:
-            self.grid[y * self.width + x] = color
-
-    def swap(self, a, b):
-        self.grid[a], self.grid[b] = self.grid[b], self.grid[a]
-
-    def is_empty(self, index):
-        return self.grid[index] == 0
-
-    def update_pixel(self, i):
-        below = i + self.width
-        below_left = below - 1
-        below_right = below + 1
-
-        if below < len(self.grid) and self.is_empty(below):
-            self.swap(i, below)
-        elif below_left < len(self.grid) and self.is_empty(below_left):
-            self.swap(i, below_left)
-        elif below_right < len(self.grid) and self.is_empty(below_right):
-            self.swap(i, below_right)
-
-    def update(self):
-        for i in range(len(self.grid) - self.width - 1, 0, -1):
-            self.update_pixel(i)
+        self.cells = [[0 for _ in range(self.height)] for _ in range(self.width)]
--- simulation.py
+++ simulation.py
@@ -1,80 +1,32 @@
 import glfw
 from OpenGL.GL import *
-import numpy as np
 from grid import Grid
+from firework import Firework
 
-def main():
-    if not glfw.init():
-        print("Failed to initialise GLFW")
-        return
+class Simulation:
+    def __init__(self, width, height, scaling_factor):
+        self.grid = Grid(width, height)
+        self.fireworks = []
+        self.scaling_factor = scaling_factor  # Grid to window size ratio
 
-    # Window dimensions
-    x_window, y_window = 200, 150
-    scaling_factor = 2
-    # Logical grid dimensions (smaller than the window)
-    grid_width = int(x_window / scaling_factor)
-    grid_height = int(y_window / scaling_factor)
+    def launch_firework(self, x, y):
+        self.fireworks.append(Firework(x, y))
 
-    window = glfw.create_window(x_window, y_window, "Fixel", None, None)
-    if not window:
-        glfw.terminate()
-        print("Failed to create GLFW window")
-        return
+    def update(self):
+        for fw in self.fireworks[:]:
+            fw.update()
+            if fw.is_exploded and not fw.particles:
+                self.fireworks.remove(fw)
 
-    glfw.make_context_current(window)
-    glfw.swap_interval(1)
-
-    grid = Grid(grid_width, grid_height)
-    grid.clear()
-
-    # Setup for rendering
-    glClearColor(0, 0, 0, 1)
-    glEnable(GL_POINT_SMOOTH)
-    point_size = scaling_factor * 2
-    glPointSize(point_size)
-
-    is_dragging = False
-
-    def on_mouse_button(window, button, action, mods):
-        nonlocal is_dragging
-        if button == glfw.MOUSE_BUTTON_LEFT:
-            if action == glfw.PRESS:
-                is_dragging = True
-                create_particle_at_mouse_pos(window)
-            elif action == glfw.RELEASE:
-                is_dragging = False
-
-    def on_mouse_move(window, xpos, ypos):
-        if is_dragging:
-            create_particle_at_mouse_pos(window)
-
-    def create_particle_at_mouse_pos(window):
-        # Scale mouse coordinates to grid coordinates
-        x, y = glfw.get_cursor_pos(window)
-        grid_x, grid_y = int(x * grid_width / x_window), int(y * grid_height / y_window)
-        grid.set(grid_x, grid_y, np.random.randint(1, 0xFFFFFF))
-
-    glfw.set_mouse_button_callback(window, on_mouse_button)
-    glfw.set_cursor_pos_callback(window, on_mouse_move)
-
-    while not glfw.window_should_close(window):
-        glfw.poll_events()
-        grid.update()
-
+    def render(self):
         glClear(GL_COLOR_BUFFER_BIT)
+        glPointSize(self.scaling_factor * 2)
         glBegin(GL_POINTS)
-        # Adjust rendering to scale grid to window size
-        for y in range(grid.height):
-            for x in range(grid.width):
-                colour = grid.grid[y * grid.width + x]
-                if colour != 0:
-                    glColor3f(((colour >> 16) & 0xFF) / 255.0, ((colour >> 8) & 0xFF) / 255.0, (colour & 0xFF) / 255.0)
-                    glVertex2f(x * 2 / grid_width - 1, 1 - y * 2 / grid_height)
+        for fw in self.fireworks:
+            if not fw.is_exploded:
+                glColor3f(1.0, 1.0, 1.0)  # White for active firework
+                glVertex2f(fw.x * 2 / self.grid.width - 1, 1 - fw.y * 2 / self.grid.height)
+            for p in fw.particles:
+                glColor3f(((p.color >> 16) & 0xFF) / 255.0, ((p.color >> 8) & 0xFF) / 255.0, (p.color & 0xFF) / 255.0)
+                glVertex2f(p.x * 2 / self.grid.width - 1, 1 - p.y * 2 / self.grid.height)
         glEnd()
-
-        glfw.swap_buffers(window)
-
-    glfw.terminate()
-
-if __name__ == "__main__":
-    main()
